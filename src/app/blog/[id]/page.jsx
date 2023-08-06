dd0abfc43134c1d5dc889a7e378a07bf4f043eae
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-08-06 15:39:36 +0300

    feat: lesson 7, fetching data

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,33 +1,43 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-const Details = () => {
-  return (
-    <div className={styles.container}>
-      {/* <div className={styles.top}>
-        <div className={styles.info}>
-          <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>{data.desc}</p>
-          <div className={styles.author}>
-            <Image
-              src={data.img}
-              alt=""
-              width={40}
-              height={40}
-              className={styles.avatar}
-            />
-            <span className={styles.username}>{data.username}</span>
-          </div>
-        </div>
-        <div className={styles.imageContainer}>
-          <Image src={data.img} alt="" fill={true} className={styles.image} />
-        </div>
-      </div>
-      <div className={styles.content}>
-        <p className={styles.text}>{data.content}</p>
-      </div> */}
-    </div>
-  );
+async function getData(id) {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {cache: "no-cache"});
+	if (!res.ok) {
+		throw new Error('Failed to fetch data')
+	}
+
+	return res.json()
+}
+
+const Details = async ({params}) => {
+	const data = await getData(params.id);
+	return (
+		<div className={styles.container}>
+			<div className={styles.top}>
+				<div className={styles.info}>
+					<h1 className={styles.title}>{data.title}</h1>
+					<p className={styles.desc}>{data.body}</p>
+					<div className={styles.author}>
+						<Image
+							src={"https://img.freepik.com/free-photo/free-photo-happy-birthday-background-wallpaper_1340-29843.jpg?size=626&ext=jpg"}
+							alt=""
+							width={40}
+							height={40}
+							className={styles.avatar}
+						/>
+						{/*<span className={styles.username}>{data.username}</span>*/}
+					</div>
+				</div>
+				<div className={styles.imageContainer}>
+					<Image src={"https://img.freepik.com/free-photo/free-photo-happy-birthday-background-wallpaper_1340-29843.jpg?size=626&ext=jpg"} alt="" fill={true} className={styles.image}/>
+				</div>
+			</div>
+			<div className={styles.content}>
+				<p className={styles.text}>{data.body}</p>
+			</div>
+		</div>
+	);
 };
 
 export default Details;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,32 +2,43 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-const Blog = () => {
-  return (
-    <div className={styles.container}>
-      {/* {data.map((item) => (
-        <Link
-          href={`blog/${item._id}`}
-          className={styles.container}
-          key={item._id}
-        >
-          <div className={styles.imageContainer}>
-            <Image
-              src={item.img}
-              alt=""
-              width={400}
-              height={250}
-              className={styles.image}
-            />
-          </div>
-          <div className={styles.content}>
-            <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.desc}</p>
-          </div>
-        </Link>
-      ))} */}
-    </div>
-  );
+async function getData() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts', {cache: "no-cache"});
+	if (!res.ok) {
+		throw new Error('Failed to fetch data')
+	}
+
+	return res.json()
+}
+
+const Blog = async () => {
+	const data = await getData()
+	console.log(data)
+	return (
+		<div className={styles.container}>
+			{data.map((item) => (
+				<Link
+					href={`blog/${item.id}`}
+					className={styles.container}
+					key={item.id}
+				>
+					<div className={styles.imageContainer}>
+						<Image
+							src={"https://img.freepik.com/free-photo/free-photo-happy-birthday-background-wallpaper_1340-29843.jpg?size=626&ext=jpg"}
+							alt=""
+							width={400}
+							height={250}
+							className={styles.image}
+						/>
+					</div>
+					<div className={styles.content}>
+						<h1 className={styles.title}>{item.title}</h1>
+						<p className={styles.desc}>{item.body}</p>
+					</div>
+				</Link>
+			))}
+		</div>
+	);
 };
 
 export default Blog;
